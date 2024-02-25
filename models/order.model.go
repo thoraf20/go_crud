8c6257112d93e77f080db0ed32b62e13297e3a1b
thoraf20 <thoraf20@gmail.com>
2024-02-25 09:30:31 +0100

    note, user, order, order item and table models

---
+++ models/order.model.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Order struct {
+	ID 									primitive.ObjectID 				`bson:'_id'`
+	Order_Date	        *time.Time                `bson:'_order_date'`
+	Created_at          *time.Time                `json:"created_at"`
+	Update_at           *time.Time                `json:"created_at"`
+	Order_id            string                    `json:"order_id"`
+	Table_id   					string     								`json:"table_id" validate:"required"`
+}
--- models/table.model.go
+++ models/table.model.go
@@ -1,1 +1,15 @@
-user
+package models
+
+import (
+	"time"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Table struct {
+	ID 									primitive.ObjectID 				`bson:'_id'`
+	Number_of_guests		*int                    	`json:'number_of_guests'`
+	Table_number        *int                    	`json:'table_number`
+	Created_at          *time.Time                `json:"created_at"`
+	Update_at           *time.Time                `json:"created_at"`
+	Table_id            string                    `json:"table_id" validate:"required"`
+}
